b366eb137f2c953afd1ffdeb36f8909d91c8809b
John Pittman <john@chickendinosaur.com>
2018-04-25 19:51:14 -0500

    Remove unnecessary jest setup script.

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,7 @@
 
 module.exports = {
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    moduleNameMapper: {
-        '\\.(css)$': 'typescript'
-    },
     projects: ['./src'],
-    setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js',
     testEnvironment: 'node',
     testRegex: '.\\.test\\.(js?|jsx?|ts?|tsx?)$',
     transform: {
